5eae82908613c4c0b36f265e823a45a3cfbac9a7
Jon Kuperman <jon.kuperman@gmail.com>
2014-01-09 22:07:53 -0500

    tidied up code for consistency

--- listify.js
+++ listify.js
@@ -13,13 +13,11 @@
 					$list = $('<ul class="listify-sublist"></ul>'),
 					tools = $this.data(attributes);
 
-				if ($this.parent().get(0) !== $listify.get(0)) {
-					return;
-				}
+				if ($this.parent().get(0) !== $listify.get(0)) { return; }
 
 				$.each(tools, function(key, value) {
 					var $tool = $('[data-'+attribute+'='+value+']');
-					if(effect === 'append') {
+					if (effect === 'append') {
 						$tool.clone().appendTo($list);
 						$tool.hide();
 					} else {
@@ -27,22 +25,20 @@
 					}
 				});
 
-				if(effect === 'append') {
-					$this.append($list);
-				}
+				if (effect === 'append') { $this.append($list); }
 			})
 			.on('mouseleave', 'li', function() {
-				if(effect === 'append') {
-					if ($(this).closest('.listify-sublist').get().length !== 0) { return; }
+				var $this = $(this);
+
+				if (effect === 'append') {
+					if ($this.closest('.listify-sublist').get().length !== 0) { return; }
 					$('li').each(function() { $(this).show(); });
 					$('.listify-sublist').remove();
 				} else {
-					var $this = $(this),
-					tools = $this.data(attributes);
+					var tools = $this.data(attributes);
 
 					$.each(tools, function(key, item) {
-						var $tool = $('[data-'+attribute+'='+item+']');
-						$tool.animate({opacity: '1.0'}, 200);
+						$('[data-'+attribute+'='+item+']').animate({opacity: '1.0'}, 200);
 					});
 				}
 		});
