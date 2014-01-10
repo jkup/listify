7d944faa95c1baf350e2d522c783da875742cfa3
Jon Kuperman <jon.kuperman@gmail.com>
2014-01-09 19:45:28 -0500

    re-factored code to accept multiple options cleanly

--- listify.js
+++ listify.js
@@ -6,49 +6,36 @@
 		var effect = (options === 'append') ? 'append' : 'toggle';
 
 		var $listify = this;
-		if(effect === 'append') {
-			$listify
-				.on('mouseenter', 'li', function() {
-					var $this = $(this),
-						$list = $('<ul class="listify-sublist"></ul>'),
-						tools = $this.data('tools');
-
-					if ($this.parent()[0] !== $listify[0]) {
-						return;
-					}
-
-					$.each(tools, function(key, value) {
-						var $tool = $('[data-tool='+value+']');
-						$tool.clone().appendTo($list);
-						$tool.hide();
-					});
-
-					$this.append($list);
-				})
-				.on('mouseleave', 'li', function() {
-					if ($(this).closest('.listify-sublist').get().length !== 0) {
-						return;
-					}
-
-					$('li').each(function() {
-						$(this).show();
-					});
-
-					$('.listify-sublist').remove();
-			});
-		} else {
-
-			$listify
-				.on('mouseenter', 'li', function() {
-					var $this = $(this),
+		$listify
+			.on('mouseenter', 'li', function() {
+				var $this = $(this),
+					$list = $('<ul class="listify-sublist"></ul>'),
 					tools = $this.data('tools');
 
-					$.each(tools, function(key, item) {
-						var $tool = $('[data-tool='+item+']');
+				if ($this.parent()[0] !== $listify[0]) {
+					return;
+				}
+
+				$.each(tools, function(key, value) {
+					var $tool = $('[data-tool='+value+']');
+					if(effect === 'append') {
+						$tool.clone().appendTo($list);
+						$tool.hide();
+					} else {
 						$tool.animate({opacity: '0.0'});
-					});
-				})
-				.on('mouseleave', 'li', function() {
+					}
+				});
+
+				if(effect === 'append') {
+					$this.append($list);
+				}
+			})
+			.on('mouseleave', 'li', function() {
+				if(effect === 'append') {
+					if ($(this).closest('.listify-sublist').get().length !== 0) { return; }
+					$('li').each(function() { $(this).show(); });
+					$('.listify-sublist').remove();
+				} else {
 					var $this = $(this),
 					tools = $this.data('tools');
 
@@ -56,7 +43,7 @@
 						var $tool = $('[data-tool='+item+']');
 						$tool.animate({opacity: '1.0'});
 					});
-				});
-		}
+				}
+		});
 	};
 })(jQuery);
