311337e9d17256f13f5c7552c012966dd53d904e
Jon Kuperman <jon.kuperman@gmail.com>
2015-02-08 23:35:58 -0800

    Add animation queues #2

--- listify.js
+++ listify.js
@@ -7,40 +7,40 @@
 		attributes = (typeof options.attribute !== 'undefined') ? options.attribute+'s' : 'tools',
 		$listify = this;
 
-		$listify
-			.on('mouseenter', 'li', function() {
-				var $this = $(this),
-					$list = $('<ul class="listify-sublist"></ul>'),
-					tools = $this.data(attributes);
+		$listify.on('mouseenter', 'li', function() {
+			var $this = $(this),
+				$list = $('<ul class="listify-sublist"></ul>'),
+				tools = $this.data(attributes);
 
-				if ($this.parent().get(0) !== $listify.get(0)) { return; }
+			if ($this.parent().get(0) !== $listify.get(0)) { return; }
 
-				$.each(tools, function(key, value) {
-					var $tool = $('[data-'+attribute+'='+value+']');
-					if (effect === 'append') {
-						$tool.clone().appendTo($list);
-						$tool.hide();
-					} else {
-						$tool.animate({opacity: '0.0'}, 200);
-					}
-				});
-
-				if (effect === 'append') { $this.append($list); }
-			})
-			.on('mouseleave', 'li', function() {
-				var $this = $(this);
-
+			$.each(tools, function(key, value) {
+				var $tool = $('[data-'+attribute+'='+value+']');
 				if (effect === 'append') {
-					if ($this.closest('.listify-sublist').get().length !== 0) { return; }
-					$('li').each(function() { $(this).show(); });
-					$('.listify-sublist').remove();
+					$tool.clone().appendTo($list);
+					$tool.hide();
 				} else {
-					var tools = $this.data(attributes);
-
-					$.each(tools, function(key, item) {
-						$('[data-'+attribute+'='+item+']').animate({opacity: '1.0'}, 200);
-					});
+					$tool.stop().animate({opacity: '0.0'}, 200);
 				}
+			});
+
+			if (effect === 'append') { $this.append($list); }
+		});
+
+		$listify.on('mouseleave', 'li', function() {
+			var $this = $(this);
+
+			if (effect === 'append') {
+				if ($this.closest('.listify-sublist').get().length !== 0) { return; }
+				$('li').each(function() { $(this).show(); });
+				$('.listify-sublist').remove();
+			} else {
+				var tools = $this.data(attributes);
+
+				$.each(tools, function(key, item) {
+					$('[data-'+attribute+'='+item+']').stop().animate({opacity: '1.0'}, 200);
+				});
+			}
 		});
 	};
 })(jQuery);
